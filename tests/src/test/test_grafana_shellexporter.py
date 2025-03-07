965337d2af35181784192ffb46b8a72a825c67c0
Ronny Friedland <Ronny.Friedland@telekom.de>
2025-03-07 09:00:50 +0100

    Add shellexporter tests

---
+++ tests/src/test/test_grafana_shellexporter.py
@@ -0,0 +1,67 @@
+import unittest
+
+import os
+import time
+
+from playwright.sync_api import Page, Playwright, expect
+import pytest
+from src.test.base import PreFlightCheck
+from src.pages import grafana
+
+now = time.time()
+
+
+class Grafana(PreFlightCheck):
+
+    def __init__(self):
+        self.url = PreFlightCheck.get_config("GRAFANA_URL")
+        self.user = PreFlightCheck.get_config("GRAFANA_USER")
+        self.password = PreFlightCheck.get_config("GRAFANA_PASSWORD")
+        self.browser = None
+
+    def setup(self, playwright: Playwright) -> Page:
+        """ Login if no context """
+        if self.browser is None:
+            self.browser = playwright.firefox.launch_persistent_context(
+                user_data_dir=f"./test_grafana_{now}",
+                headless=os.getenv("HEADLESS") in ("true", "True", "TRUE"))
+        page = self.browser.pages[0]
+
+        page.goto(self.url)
+
+        grafana.login(page, self.user, self.password)
+
+        return page
+
+
+subject = Grafana()
+
+
+@pytest.mark.base
+def test_dashboards_exists(playwright: Playwright) -> None:
+    page = subject.setup(playwright)
+
+    grafana.open_dashboards(page)
+
+    expect(page.get_by_role("link", name="shellexporter")).to_be_visible()
+
+
+@pytest.mark.base
+def test_dashboard_contains_panel(playwright: Playwright) -> None:
+    page = subject.setup(playwright)
+
+    grafana.open_dashboard(page, "shellexporter")
+
+    panel = page.get_by_test_id(f"data-testid Panel header bash_gauge.sh").get_by_role("heading",
+                                                                                         name="bash_gauge.sh")
+    expect(panel).to_be_visible()
+
+
+@pytest.mark.base
+def test_panel(playwright: Playwright) -> None:
+    page = subject.setup(playwright)
+
+    grafana.open_dashboard(page, "shellexporter")
+
+    for i in range(1, 9):
+        expect(page.get_by_text(f"for_{i}", exact=True)).to_be_visible()
