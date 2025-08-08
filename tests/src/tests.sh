86f33a32ee2d68d7df014c9e7496a630a52186ca
Frank Birkhofen <frank.birkhofen@telekom.de>
2025-08-08 14:29:02 +0200

    add proposed changes

--- tests/src/tests.sh
+++ tests/src/tests.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
-poetry run pytest --verbose /tests/src/test/test_demoapp.py & sleep 60   # give some time to metrics shipping and calculation
+poetry run pytest --verbose /tests/src/test/test_demoapp.py
+
+echo "waiting 60 seconds to give some time to metrics shipping and calculation" && sleep 60
+
 poetry run pytest --verbose /tests/src/test/test_grafana_demoapp.py
 poetry run pytest --verbose /tests/src/test/test_grafana_shellexporter.py
