e41f90041813bfd2886cd1f44fb2cd9c9d2f90a8
Frank B. <95924851+2Fraennk@users.noreply.github.com>
2025-08-11 13:12:00 +0200

    Update tests.sh

--- tests/src/tests.sh
+++ tests/src/tests.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 poetry run pytest --verbose /tests/src/test/test_demoapp.py
+echo "'$? :' $?" 
 
 echo "waiting 60 seconds to give some time to metrics shipping and calculation" && sleep 60
 
 poetry run pytest --verbose /tests/src/test/test_grafana_demoapp.py
+echo "'$? :' $?" 
+
 poetry run pytest --verbose /tests/src/test/test_grafana_shellexporter.py
+echo "'$? :' $?" 
