7b40305b3cfcaf442cf063270a15b3434e78bfda
Frank Birkhofen <frank.birkhofen@telekom.de>
2025-08-13 14:44:15 +0200

    update actions to analyze test report

--- tests/src/tests.sh
+++ tests/src/tests.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 poetry run pytest --verbose /tests/src/test/test_demoapp.py
-echo "'$? :' $?" 
+if [ "$rc" -gt 0 ]; then exit 1; fi
 
 echo "waiting 60 seconds to give some time to metrics shipping and calculation" && sleep 60
 
 poetry run pytest --verbose /tests/src/test/test_grafana_demoapp.py
-echo "'$? :' $?" 
+if [ "$rc" -gt 0 ]; then exit 1; fi
 
 poetry run pytest --verbose /tests/src/test/test_grafana_shellexporter.py
-echo "'$? :' $?" 
+if [ "$rc" -gt 0 ]; then exit 1; fi
